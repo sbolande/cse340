7184e5c361478412e957ea1df469ac08675fc1f6
Seth Bolander <bol15015@byui.edu>
2021-09-24 22:01:06 -0600

    Removed header.php include from 500

--- www/phpmotors/view/500.php
+++ www/phpmotors/view/500.php
@@ -5,7 +5,20 @@
         <link rel="stylesheet" type="text/css" href="../css/style.css" media="screen">
     </head>
     <body>
-        <? require_once($_SERVER['DOCUMENT_ROOT'] . "/phpmotors/view/modules/header.php"); ?>
+        <header>
+            <div id="header-top">
+                <img src="http://lvh.me/phpmotors/images/site/logo.png" id="logo" alt="PHP Motors">
+                <a id="account" href="" title="My Account">My Account</a>
+            </div>
+            <ul id="navbar">
+                <li><a class="navbar-item" href="/phpmotors/index.php" title="View the PHP Motors home page">Home</a></li>
+                <li><a class="navbar-item" href="" title="View our Classic product line">Classic</a></li>
+                <li><a class="navbar-item" href="" title="View our Sports product line">Sports</a></li>
+                <li><a class="navbar-item" href="" title="View our SUV product line">SUV</a></li>
+                <li><a class="navbar-item" href="" title="View our Trucks product line">Trucks</a></li>
+                <li><a class="navbar-item" href="" title="View our Used product line">Used</a></li>
+            </ul>
+        </header>
         <div id="content">
             <h1>Server Error</h1>
             <p class="content__data">Sorry our server seems to be experiencing some technical difficulties</p>
