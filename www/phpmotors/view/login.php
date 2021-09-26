d1c188417584686ceb6d85bbe0e71e9b1f7feca7
Seth Bolander <bol15015@byui.edu>
2021-09-25 23:20:13 -0600

    Updates to fix HTML

--- www/phpmotors/view/login.php
+++ www/phpmotors/view/login.php
@@ -9,32 +9,33 @@
         <div id="content">
             <h1>Login</h1>
             <div class="content__data">
-                <form class="login" method="POST" action="" name="login">
+                <form class="form" method="POST" action="javascript:void(0);" name="login">
                     <ul>
                         <li>
                             <label for="email">Email</label>
-                            <input type="email" name="email" required>
+                            <input type="email" name="email" id="email" required>
                         </li>
                         <li>
                             <label for="password">Password</label>
-                            <input type="password" name="password" required>
+                            <input type="password" name="password" id="password" required>
                             <span id="showPass" class="hint" onclick="togglePassword()">Show Password</span>
                         </li>
                         <li><input id="submit" type="submit" value="Login"></li>
                     </ul>
                 </form>
+                <div>No account? <a id="alt-form-link" href="/phpmotors/index.php?action=signup" title="Register for an account">Sign Up</a></div>
             </div>
         </div>
         <? require_once($_SERVER['DOCUMENT_ROOT'] . "/phpmotors/view/modules/footer.php"); ?>
+        <script>
+            function togglePassword() {
+                let password = document.forms['login'].elements['password'];
+                password.setAttribute('type', password.getAttribute('type') === 'password' ? 'text' : 'password');
+            };
+            // set first input to focused element or active element
+            var firstInput = document.forms['login'].elements[0];
+            if (firstInput.setActive) firstInput.setActive();
+            else if (firstInput.focus) firstInput.focus();
+        </script>
     </body>
-    <script type="text/javascript">
-        function togglePassword() {
-            let password = document.forms['login'].elements['password'];
-            password.setAttribute('type', password.getAttribute('type') === 'password' ? 'text' : 'password');
-        };
-        // set first input to focused element or active element
-        var firstInput = document.forms['login'].elements[0];
-        if (firstInput.setActive) firstInput.setActive();
-        else if (firstInput.focus) firstInput.focus();
-    </script>
 </html>
