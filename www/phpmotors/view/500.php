08f781aaefcd331e8423ec70c13b11ba12cccd8d
Seth Bolander <bol15015@byui.edu>
2021-11-06 23:02:55 -0600

    Updated date

--- www/phpmotors/view/500.php
+++ www/phpmotors/view/500.php
@@ -27,7 +27,7 @@
     </div>
     <footer>
         <hr>
-        &#169; PHP Motors, All rights reserved.<br>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.<br>Last Updated: 3 November, 2021
+        &#169; PHP Motors, All rights reserved.<br>All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.<br>Last Updated: 6 November, 2021
     </footer>
 </body>
 
