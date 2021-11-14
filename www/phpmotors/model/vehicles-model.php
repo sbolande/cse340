e4826cb45952504edcab068ce46f63b64e46d5a8
Seth Bolander <bol15015@byui.edu>
2021-11-13 23:15:28 -0700

    finished building uploads controller

--- www/phpmotors/model/vehicles-model.php
+++ www/phpmotors/model/vehicles-model.php
@@ -136,4 +136,16 @@
         $stmt->closeCursor();
         return $rowsChanged;
     }
+
+    // GET VEHICLES
+    // Get information for all vehicles
+    function getVehicles() {
+        $db = phpmotorsConnect();
+        $sql = 'SELECT invId, invMake, invModel FROM inventory';
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $invInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt->closeCursor();
+        return $invInfo;
+    }
 ?>
