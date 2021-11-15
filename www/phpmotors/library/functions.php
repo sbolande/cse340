52641be0a69e105ef1fd71c860e3d18bd99d7a3a
Seth Bolander <bol15015@byui.edu>
2021-11-15 10:18:27 -0700

    updated styling for image display

--- www/phpmotors/library/functions.php
+++ www/phpmotors/library/functions.php
@@ -106,7 +106,7 @@
         foreach ($imageArray as $image) {
             $id .= '<li>';
             $id .= "<img src='$image[imgPath]' title='$image[invMake] $image[invModel] image on PHP Motors.com' alt='$image[invMake] $image[invModel] image on PHP Motors.com'>";
-            $id .= "<p><a href='/phpmotors/uploads?action=delete&imgId=$image[imgId]&filename=$image[imgName]' title='Delete the image'>Delete $image[imgName]</a></p>";
+            $id .= "<p><a href='/phpmotors/uploads?action=delete&imgId=$image[imgId]&filename=$image[imgName]' title='Delete this image'>Delete $image[imgName]</a></p>";
             $id .= '</li>';
         }
         $id .= '</ul>';
